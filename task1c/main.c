cc618631c4e10f7f947306bd7bae474273591c1f
daniel-ubuntu <elhadan@post.bgu.ac.il>
2022-03-31 20:29:31 +0300

    test

---
+++ task1c/main.c
@@ -0,0 +1,17 @@
+//#include <stdio.h>
+//#include <string.h>
+//#include <stdlib.h>
+//
+//int main (int argc, char** argv){
+//    int iarray[3];
+//    float farray[3];
+//    double darray[3];
+//    char carray[3];
+//    for (int i = 0; i < 3; ++i) {
+//        printf("- &iarray[]: %p\n",&iarray[i]);
+//        printf("- &farray[]: %p\n",&farray[i]);
+//        printf("- &darray[]: %p\n",&darray[i]);
+//        printf("- &carray[]: %p\n",&carray[i]);
+//
+//    }
+//}
---
+++ task1d/task1d.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main (int argc, char** argv){
+    int iarray[] = {1,2,3};
+    char carray[] = {'a','b','c'};
+    int* iarrayPtr = &iarray[0];
+    char* carrayPtr = &carray[0];
+    for (int i = 0; i < 3; ++i) {
+        printf("%c",*(carrayPtr + i));
+    }
+}
