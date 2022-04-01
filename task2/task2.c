81b0533c187f60ac0c462b488f5b239e9686cf4c
daniel-ubuntu <elhadan@post.bgu.ac.il>
2022-04-01 10:23:44 +0300

    task2b done

--- task2/task2.c
+++ task2/task2.c
@@ -26,12 +26,36 @@ char encrypt(char c) {
     return c;
 }
 
+char decrypt(char c) {
+    if (c >= 41 && c <= 122) {
+        c = c - 2;
+    }
+    return c;
+}
+
+char dprt(char c) {
+    printf("%d\n",c);
+    return c;
+}
+
+char cprt(char c) {
+    if (c >= 41 && c <= 122)
+        printf("%c\n", c);
+    else
+        printf("%c\n", '*');
+    return c;
+}
+
+char my_get(char c) {
+    c = (fgetc(stdin));
+    return c;
+}
 
 
 
 int main(int argc, char **argv){
     char arr1[] = {'H','e','y','!'};
-    char* arr2 = map(arr1, 4, encrypt);
+    char* arr2 = map(arr1, 4, my_get);
     printf("%s\n", arr2);
     free(arr2);
 }
