78779edd48fc61f548f12dbf64d4ffe9b6e50b94
daniel-ubuntu <elhadan@post.bgu.ac.il>
2022-04-02 14:20:02 +0300

    Done

--- task2/task2.c
+++ task2/task2.c
@@ -8,19 +8,19 @@ struct fun_desc {
 };
 
 
-char censor(char c) {
-    if(c == '!')
-        return '*';
-    else
-        return c;
-}
-
 char* map(char *array, int array_length, char (*f) (char)){
     char* mapped_array = (char*)(malloc(array_length*sizeof(char)));
     for (int i = 0; i < array_length; ++i) {
         *(mapped_array + i) = f(*(array + i));
     }
     return mapped_array;
+}
+
+char censor(char c) {
+    if(c == '!')
+        return '*';
+    else
+        return c;
 }
 
 char encrypt(char c) {
@@ -52,6 +52,8 @@ char cprt(char c) {
 
 char my_get(char c) {
     c = (fgetc(stdin));
+    if(c == '\n')
+        c = '\0';
     return c;
 }
 
@@ -61,12 +63,30 @@ char quit(char c) {
     return c;
 }
 
+struct fun_desc menu[] = { { "Censor", censor }, { "Encrypt", encrypt }, { "Decrypt", decrypt },{ "Print dec", dprt },{ "Print string", cprt },{ "Get string", my_get },{ "Quit", quit }, { NULL, NULL }  };
 
 
 int main(int argc, char **argv){
-    char arr1[5] = {'\0','\0','\0','\0','\0'};
-    struct fun_desc menu[] = { { "Censor", censor }, { "Encrypt", encrypt }, { "Decrypt", decrypt },{ "Print dec", dprt },{ "Print string", cprt },{ "Get string", my_get },{ "Quit", quit }, { NULL, NULL }  };
-    char* arr2 = map(arr1, 4, encrypt);
-    printf("%s", arr2);
-    free(arr2);
+    int c;
+    char carry[5] = {'\0','\0','\0','\0','\0'};
+    while (1 != 0) {
+        printf("please choose a function:\n");
+        for (int i = 0; i < 7; ++i) {
+            printf("%d) %s\n",i,menu[i].name);
+        }
+        printf("option: ");
+        c = (fgetc(stdin)) - 48;
+        fgetc(stdin);
+        if (c < 0 || c > 6) {
+            printf("Not within bounds");
+            free(carry);
+            return 0;
+        }
+        printf("Within bounds\n");
+        char* carry = map(carry, 5, menu[c].fun);
+        printf("DONE\n");
+
+    }
+
+
 }
